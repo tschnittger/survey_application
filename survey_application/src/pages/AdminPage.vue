e8556afdb467e594af5e80d1acdef117ede4ef96
tschnittger <toni_mathias.schnittger@fh-bielefeld.de>
2024-02-22 17:06:25 +0100

    Added funtionality to create a question to adminpage
    The question is already made into a json, but not saved yet

--- survey_application/src/pages/AdminPage.vue
+++ survey_application/src/pages/AdminPage.vue
@@ -14,102 +14,289 @@
         <q-btn @click="$router.push('survey')">Back</q-btn>
       </q-toolbar>
     </q-header>
-    <q-body>
+    <body>
+      <q-dialog v-model="alert_confirm">
+        <q-card>
+          <q-card-section>
+            <div class="text-h6">Sind Sie mit dieser Frage zufrieden?</div>
+          </q-card-section>
 
-      <q-dialog v-model="alert">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">Sind Sie mit dieser Frage zufrieden?</div>
-        </q-card-section>
+          <q-card-section class="q-pt-none"> frage </q-card-section>
 
-        <q-card-section class="q-pt-none">
-          frage
-        </q-card-section>
-
-        <q-card-actions align="right">
-          <q-btn flat label="OK" color="primary" v-close-popup />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
+          <q-card-actions align="right">
+            <q-btn flat label="OK" color="primary" v-close-popup />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
 
       <div id="new_question_container">
         <div id="new_question_input">
-          <div class="q-pa-md" >
-            <div id="question_type_container" class="q-gutter-md">
-              <q-select @update:model-value="onTypeChanged($event)" v-model="model" :options="options" label="Typ:" />
+          <q-form>
+            <div class="q-pa-md q-gutter-sm">
+              <q-banner
+                id="failed_alert"
+                inline-actions
+                dense
+                class="bg-negative text-white"
+                v-show="failed_model"
+              >
+                {{ failedDialogText }}
+                <template v-slot:action>
+                  <q-btn
+                    flat
+                    color="white"
+                    label="Dismiss"
+                    @click="closeFailedAlert()"
+                  />
+                </template>
+              </q-banner>
             </div>
-            <div id="question_container">
-              <span class="label">Frage:</span>
-              <q-input filled v-model="question_text" label="Filled" />
-            </div>
-            <q-slide-transition>
-              <div v-show="visible">
-                <div id="answer_container" class="form-group" v-for="(answer, k) in answers" :key="k" >
-                  <span class="label">Antwort:</span>
-                  <div id="answer_line">
-                    <q-input class="form-control" filled v-model="answer.text" label="Filled" type="text"/>
-                    <span>
-                      <q-btn @click="addAnwser()" v-show="k == answers.length-1">+</q-btn>
-                      <q-btn @click="removeAnswer(k)" v-show="k || ( !k && answers.length > 1)">-</q-btn>
-                    </span>
+            <div class="q-pa-md">
+              <div id="question_type_container" class="q-gutter-md">
+                <q-select
+                  id="question_type"
+                  @update:model-value="onTypeChanged($event)"
+                  v-model="model"
+                  :options="options"
+                  label="Typ:"
+                  :rules="[(val) => val.length > 0]"
+                />
+              </div>
+              <div id="question_container">
+                <span class="label">Frage:</span>
+                <q-input
+                  id="question_text_input"
+                  filled
+                  v-model="question_text"
+                  label="Filled"
+                  :rules="[(val) => val.length > 0]"
+                />
+              </div>
+              <q-slide-transition>
+                <div v-show="visible">
+                  <div
+                    id="answer_container"
+                    class="form-group"
+                    v-for="(answer, k) in answers"
+                    :key="k"
+                  >
+                    <span class="label">Antwort:</span>
+                    <div id="answer_line">
+                      <q-input
+                        id="answer_input"
+                        @input="handleInput($event)"
+                        class="form-control"
+                        v-model="answer.text"
+                        label="Filled"
+                        type="text"
+                        :rules="[(val) => val.length > 0]"
+                      />
+
+                      <span>
+                        <q-btn
+                          @click="addAnwser()"
+                          v-show="k == answers.length - 1"
+                          >+</q-btn
+                        >
+                        <q-btn
+                          @click="removeAnswer(k)"
+                          v-show="k || (!k && answers.length > 2)"
+                          >-</q-btn
+                        >
+                      </span>
+                    </div>
+                  </div>
+                  <div id="multiple_choice_container">
+                    <q-checkbox
+                      id="multiple_choice"
+                      left-label
+                      v-model="multiple_choice_checkbox"
+                      label="Mehrere Antworten möglich?"
+                    />
+                  </div>
+                  <div id="custon_answer_container">
+                    <q-checkbox
+                      id="custom_question"
+                      left-label
+                      v-model="custom_question_checkbox"
+                      label="Eigene Antwort hinzufügen?"
+                    />
                   </div>
                 </div>
-                <div id="multiple_choice_container">
-                  <q-checkbox left-label v-model="multiple_choice_checkbox" label="Mehrere Antworten möglich?" />
-                </div>
-                <div id="custon_answer_container">
-                  <q-checkbox left-label v-model="custom_question_checkbox" label="Eigene Antwort hinzufügen?" />
-                </div>
+              </q-slide-transition>
+              <div id="submit_container">
+                <q-btn @click="onSubmit()" type="submit">Absenden</q-btn>
               </div>
-            </q-slide-transition>
-            <div id="submit_container">
-              <q-btn @click="onSubmit()">Absenden</q-btn>
             </div>
-          </div>
+          </q-form>
         </div>
       </div>
-    </q-body>
+    </body>
     <q-drawer v-model="leftDrawerOpen" bordered>
-      <q-list>
-      </q-list>
+      <q-list> </q-list>
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
-
 </template>
 
 <script>
 import { ref } from 'vue';
 
 var leftDrawerOpen = ref(false);
+const first_ID = 1;
+var curr_id = first_ID;
+var type_options = ['Multiple-Choice Frage', 'Textfrage'];
+var failed_dialog = [
+  'Ein unbekannter Fehler ist aufgetreten, bitte versuchen Sie es erneut.',
+  'Bitte wählen Sie einen Fragen-Typ aus.',
+  'Bitte Geben Sie eine Frage ein.',
+  'Bitte Geben Sie mindesten 2 Antworten an.',
+  'Die Antworten dürfen nicht leer sein.',
+];
+var curr_failed_dialog = 0;
+var is_failed_visible = false;
+
+function buildQuestion(
+  type,
+  question,
+  answers,
+  multiple_choice,
+  custom_question
+) {
+  if (validateData(type, question, answers)) {
+    //Build JSON based on questiontype
+    if (type == type_options[1]) {
+      //Build JSON containing question data
+      const data = JSON.stringify({
+        ID: curr_id,
+        TYP: type,
+        QUESTION: question,
+      });
+      console.log(data);
+      return data;
+    } else {
+      //Extract answers into a more direct array for easier access
+      var ans = [];
+      for (var i = 0; i < answers.length; i++) {
+        ans[i] = answers[i].text;
+      }
+      //Build JSON containing question data
+      const data = JSON.stringify({
+        ID: curr_id,
+        TYP: type,
+        MC: multiple_choice,
+        CQ: custom_question,
+        QUESTION: question,
+        answers: ans,
+      });
+      console.log(data);
+      return data;
+    }
+  } else {
+    return null;
+  }
+}
+
+/**
+ * The function validates the input data of the form and sets the appropriate error message if necessary
+ *
+ * @param {*} type Question type, needs to be an entry of "type_options" array
+ * @param {*} question The Question itself, can't be empty
+ * @param {*} answers Answers for the question, if the question is multiple-choice-type, they cant be empty and there must be at least 2
+ */
+function validateData(type, question, answers) {
+  if (validateType(type)) {
+    if (validateQuestion(question)) {
+      if (validateAnswers(type, answers)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+/**
+ * Validates the question-type, returns false and sets the appropriate error message if necessary
+ * @param {*} type Question type, needs to be an entry of "type_options" array
+ */
+function validateType(type) {
+  var temp_verification = false;
+  for (var i = 0; i < type_options.length; i++) {
+    if (type == type_options[i]) {
+      temp_verification = true;
+    }
+  }
+  if (temp_verification == false) {
+    curr_failed_dialog = 1;
+    return false;
+  } else {
+    return true;
+  }
+}
+
+/**
+ * Validates the question, returns false and sets the appropriate error message if necessary
+ * @param {*} question Question itself, can't be empty
+ */
+function validateQuestion(question) {
+  if (question == '') {
+    curr_failed_dialog = 2;
+    return false;
+  } else {
+    return true;
+  }
+}
+
+/**
+ * Validates the answers, returns false and sets the appropriate error message if necessary
+ * @param {*} type Question-type, to check if the question needs answers
+ * @param {*} answers Answers for the question, if the question is multiple-choice-type, they cant be empty and there must be at least 2
+ */
+function validateAnswers(type, answers) {
+  for (var i = 0; i < answers.length; i++) {
+    if (answers[i].text == '' && type == type_options[0]) {
+      curr_failed_dialog = 4;
+      return false;
+    }
+  }
+  if (answers.length < 2 && type == type_options[0]) {
+    curr_failed_dialog = 3;
+    return false;
+  }
+  return true;
+}
 
 export default {
+  //Add a second answer-prompt when the page is loaded, since there always have to be at least 2.
+  beforeMount() {
+    this.addAnwser();
+  },
   //Sets values to default when the page is loaded
-  setup () {
+  setup() {
     return {
       leftDrawerOpen,
-      model: ref('Multiple-Choice Frage'),
-      options: [
-      'Multiple-Choice Frage', 'Textfrage'
-      ],
+      model: ref(''),
+      options: ['Multiple-Choice Frage', 'Textfrage'],
       multiple_choice_checkbox: ref(false),
       question_text: ref(''),
       custom_question_checkbox: ref(false),
       visible: ref(true),
-      alert: ref(false)
-    }
+      alert_confirm: ref(false),
+      failed_model: ref(is_failed_visible),
+      failedDialogText: ref('Penis'),
+    };
   },
   //Inherits the current answers
   data() {
-    return{
-      answers:[
+    return {
+      answers: [
         {
-          text: ""
-        }
-      ]
+          text: '',
+        },
+      ],
     };
   },
   methods: {
@@ -118,27 +305,44 @@ export default {
       leftDrawerOpen.value = !leftDrawerOpen.value;
     },
     //Changes visibility according to question-type
-    onTypeChanged(selectedType){
-      console.log(selectedType)
-      if(selectedType == 'Textfrage'){
-        this.visible = false
-      }else{
-        this.visible = true
+    onTypeChanged(selectedType) {
+      if (selectedType == 'Textfrage') {
+        this.visible = false;
+      } else {
+        this.visible = true;
       }
     },
     //Adds a new answer
-    addAnwser(){
-      this.answers.push({text:""})
+    addAnwser() {
+      this.answers.push({ text: '' });
     },
     //Removes an answer
-    removeAnswer(index){
+    removeAnswer(index) {
       this.answers.splice(index, 1);
     },
-    //Spawnes the verification popup on submit. Later the logic to save the question will be added here
-    onSubmit(){
-      this.alert = true
-    }
-  }
+    //Spawnes the verification popup on submit. Later the logic to save the question will be called here
+    onSubmit() {
+      var QuestionData = buildQuestion(
+        this.model,
+        this.question_text,
+        this.answers,
+        this.multiple_choice_checkbox,
+        this.custom_question_checkbox
+      );
+      if (QuestionData == null) {
+        this.failedDialogText = failed_dialog[curr_failed_dialog];
+        this.failed_model = true;
+      }else{
+        curr_id++
+      }
+      this.alert_confirm = true
+    },
+    handleInput(event) {
+      answers.push(event);
+    },
+    closeFailedAlert() {
+      this.failed_model = false;
+    },
+  },
 };
-
 </script>
